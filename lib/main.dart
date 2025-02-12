cb6becc030e4dc4f9f6f139e8762529d60ffad7a
kongpda <kongpda@gmail.com>
2025-02-12 14:26:51 +0700

    update to add route and auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project_ii/api_module/event_provider.dart';
+import 'package:flutter_project_ii/api_module/event_logic.dart';
+import 'package:flutter_project_ii/auth/auth.dart';
+import 'package:flutter_project_ii/auth/login_screen.dart';
+import 'package:flutter_project_ii/auth/screens/forgot_password_screen.dart';
+import 'package:flutter_project_ii/auth/signup_screen.dart';
 import 'package:flutter_project_ii/category_module/category_service.dart';
+import 'package:flutter_project_ii/main_screen.dart';
 import 'package:flutter_project_ii/profile_module/language_logic.dart';
+import 'package:flutter_project_ii/profile_module/profile_screen.dart';
 import 'package:flutter_project_ii/start_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(provider());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-Widget provider(){
-  return MultiProvider(providers: [
-    ChangeNotifierProvider(create: (create) => LanguageLogic()),
-    ChangeNotifierProvider(create: (create) => CategoryLogic()),
+  final authProvider = AuthProvider();
+  await authProvider.init();
 
-  ],
-  child: MyApp(),
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: authProvider,
+        ),
+        ChangeNotifierProvider(create: (create) => LanguageLogic()),
+        ChangeNotifierProvider(create: (create) => CategoryLogic()),
+        ChangeNotifierProvider(create: (create) => EventLogic()),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GetStartScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF1A1B1E),
+      ),
+      routes: {
+        '/home': (context) => const MainScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/forgot-password': (context) => const ForgotPasswordScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/profile': (context) => const ProfileScreen(),
+      },
+      home: Consumer<AuthProvider>(
+        builder: (context, authProvider, child) {
+          return authProvider.isAuthenticated
+              ? const MainScreen()
+              : const LoginScreen();
+        },
+      ),
     );
   }
 }
